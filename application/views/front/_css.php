c07ce9dc06b87e4149ea15fa049910865a0c8088
Haritha Akella <harithaakella@u.boisestate.edu>
2015-10-14 11:37:37 -0500

    After enabling https
    have enabled https

---
+++ application/views/front/_css.php
@@ -0,0 +1,11 @@
+	<!-- Bootstrap -->
+	<link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
+	<!-- Icons -->
+	<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<!-- Fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,700">
+	<!-- Custom styles -->
+	<!--<link rel="stylesheet" href="<?php echo site_url('assets/css/styles.css')?>">-->
+	<link rel="stylesheet" href="https://localhost/my_new_blog/assets/css/styles.css">
+
+	<!--[if lt IE 9]> <script src="<?php echo site_url('assets/js/html5shiv.js')?>"></script> <![endif]-->
